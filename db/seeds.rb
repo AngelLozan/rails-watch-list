9eab5897f28ba3503963cae488139646bc749584
Scott Lozano <scottloz@protonmail.com>
2023-06-01 18:42:41 +0100

    removes token

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ http.use_ssl = true
 
 request = Net::HTTP::Get.new(url)
 request["accept"] = 'application/json'
-request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2QyNDA2MDNhMmEwMGZhZGJiMGU5ZGZhY2E2NDQyYSIsInN1YiI6IjY0Nzg2YjgwY2Y0YjhiMDBlMmQ0NmJlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SiFmx1MR8aO6gHdMBl4ht7qctNQn1U6_FBqaX64SyUY'
+request["Authorization"] = 'Bearer '
 
 response = http.request(request)
 response = JSON.parse(response.read_body) # type: Hash
